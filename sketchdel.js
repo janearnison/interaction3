cddd4dada8b00d4e83e1e26320d94ae69ce02fcb
janearnison <94143682+janearnison@users.noreply.github.com>
2024-04-19 16:13:22 +0200

    fixed del and touch

--- sketchdel.js
+++ sketchdel.js
@@ -1,10 +1,10 @@
 /**
  *  @name Delay
- *  @arialabel When the user clicks their mouse on the black screen, music plays, and a lime green rectangle appears from the bottom at a height level correlating with the amplitude of the sound as it plays
+ *  @arialabel When the user touches the black screen, music plays, and a lime green rectangle appears from the bottom at a height level correlating with the amplitude of the sound as it plays
  *  @description
- *  Click the mouse to hear the p5.Delay process a SoundFile.
- *  MouseX controls the p5.Delay Filter Frequency.
- *  MouseY controls both the p5.Delay Time and Resonance.
+ *  Touch the screen to hear the p5.Delay process a SoundFile.
+ *  TouchX controls the p5.Delay Filter Frequency.
+ *  TouchY controls both the p5.Delay Time and Resonance.
  *  Visualize the resulting sound's volume with an Amplitude object.
  *
  * <p><em><span class="small"> To run this example locally, you will need the
@@ -20,7 +20,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
 
   soundFile.disconnect(); // so we'll only hear delay
 
@@ -42,16 +42,18 @@ function draw() {
   fill(100, 0, 100);
   rect(0, height, width, -levelHeight);
 
-  let filterFreq = map(mouseX, 0, width, 60, 15000);
-  filterFreq = constrain(filterFreq, 60, 15000);
-  let filterRes = map(mouseY, 0, height, 3, 0.01);
-  filterRes = constrain(filterRes, 0.01, 3);
-  delay.filter(filterFreq, filterRes);
-  let delTime = map(mouseY, 0, width, 0.2, 0.01);
-  delTime = constrain(delTime, 0.01, 0.2);
-  delay.delayTime(delTime);
+  if (touches.length > 0) {
+    let filterFreq = map(touches[0].x, 0, width, 60, 15000);
+    filterFreq = constrain(filterFreq, 60, 15000);
+    let filterRes = map(touches[0].y, 0, height, 3, 0.01);
+    filterRes = constrain(filterRes, 0.01, 3);
+    delay.filter(filterFreq, filterRes);
+    let delTime = map(touches[0].y, 0, width, 0.2, 0.01);
+    delTime = constrain(delTime, 0.01, 0.2);
+    delay.delayTime(delTime);
+  }
 }
 
-function mousePressed() {
+function touchStarted() {
   soundFile.play();
 }
