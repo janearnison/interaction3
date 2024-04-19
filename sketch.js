1bee0f7c62bb278bafb4bb9a1ccbdd31f6efba7f
janearnison <94143682+janearnison@users.noreply.github.com>
2024-04-19 14:28:05 +0200

    updatesound and button

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ function preload() {
   sounds.push(loadSound('sounds/water1.mp3')); 
   sounds.push(loadSound('sounds/water2.mp3'));
   sounds.push(loadSound('sounds/water3.mp3'));
-  
+  sounds.push(loadSound('sounds/water4.mp3'));
+  sounds.push(loadSound('sounds/water5.mp3'));
 }
 
 
@@ -21,14 +22,14 @@ function setup(){
     noCursor(); 
   
   shapeColor = color(245, 150, 90);
-
-  const audioArray = ['sounds/water1.mp3', 'sounds/water2.mp3', 'water3.mp3', 'water4.mp3'];
+5
+  const audioArray = ['sounds/water1.mp3', 'sounds/water2.mp3', 'sounds/water3.mp3', 'sounds/water4.mp3', 'sounds/water5.mp3'];
 
 }
 
 
 function draw() {
-  background(187, 10, 247);
+  background(0, 0, 0);
   
 	noStroke();
   fill(shapeColor);
